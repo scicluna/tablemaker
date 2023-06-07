b343f458571a1f83a39ced81260fdaeb104a2ba0
scicluna <88948700+scicluna@users.noreply.github.com>
2023-06-07 18:55:15 -0400

    table basic complete

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,7 +1,22 @@
+import { Table } from "./TableSpace"
 
+type TableProps = {
+    currentTable: Table
+}
 
-export default function Table() {
+export default function Table({ currentTable }: TableProps) {
+
     return (
-        <h1>Table</h1>
+        <>
+            <h1 className="font-extrabold w-full text-center">{currentTable.header}</h1>
+            <div className="flex flex-col gap-1">
+                {currentTable.items.map((item, i) => (
+                    <div className="flex gap-5 sm:text-sm text-xs" key={item}>
+                        <p className="font-bold">{item && i + 1}</p>
+                        <p>{item && item}</p>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
--- components/TableSpace.tsx
+++ components/TableSpace.tsx
@@ -3,13 +3,28 @@ import DropDown from "./DropDown"
 import Table from "./Table"
 import Roller from "./Roller"
 import tables from "../inputs/tables.json"
+import { useState, useEffect } from "react"
 
+export type Table = {
+    header: string
+    items: string[]
+}
 
 export default function TableSpace() {
+    const [tableList, setTableList] = useState<{}[]>([])
+    const [currentTable, setCurrentTable] = useState<Table>()
+
+    useEffect(() => {
+        setTableList(tables)
+        setCurrentTable(tables[0])
+    }, [])
+
+    console.log(currentTable)
+
     return (
         <>
             <DropDown />
-            <Table />
+            {currentTable ? <Table currentTable={currentTable} /> : <h1>Loading...</h1>}
             <Roller />
         </>
     )
